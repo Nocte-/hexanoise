ecff4b0e99152c208833fdbf00ff477a041a77cc
Nocte- <nocte.unicorn@gmail.com>
2014-04-13 21:52:01 +0200

    Precompiled tokenizer and parser

---
+++ win32/README.md
@@ -0,0 +1,4 @@
+
+In case you're building on a Windows machine and you don't want to install
+Flex and Bison, you can grab these precompiled files.
+
---
+++ win32/parser.hpp
@@ -0,0 +1,113 @@
+/* A Bison parser, made by GNU Bison 2.7.12-4996.  */
+
+/* Bison interface for Yacc-like parsers in C
+   
+      Copyright (C) 1984, 1989-1990, 2000-2013 Free Software Foundation, Inc.
+   
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+   
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+   
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
+
+/* As a special exception, you may create a larger work that contains
+   part or all of the Bison parser skeleton and distribute that work
+   under terms of your choice, so long as that work isn't itself a
+   parser generator using the skeleton or a modified version thereof
+   as a parser skeleton.  Alternatively, if you modify or redistribute
+   the parser skeleton itself, you may (at your option) remove this
+   special exception, which will cause the skeleton and the resulting
+   Bison output files to be licensed under the GNU General Public
+   License without this special exception.
+   
+   This special exception was added by the Free Software Foundation in
+   version 2.2 of Bison.  */
+
+#ifndef YY_YY_HOME_UNICORN_DEVEL_SSD_HEXANOISE_BUILD_HEXANOISE_PARSER_HPP_INCLUDED
+# define YY_YY_HOME_UNICORN_DEVEL_SSD_HEXANOISE_BUILD_HEXANOISE_PARSER_HPP_INCLUDED
+/* Enabling traces.  */
+#ifndef YYDEBUG
+# define YYDEBUG 0
+#endif
+#if YYDEBUG
+extern int yydebug;
+#endif
+/* "%code requires" blocks.  */
+/* Line 2053 of yacc.c  */
+#line 20 "parser.y"
+
+
+#ifndef YY_TYPEDEF_YY_SCANNER_T
+#define YY_TYPEDEF_YY_SCANNER_T
+typedef void* yyscan_t;
+#endif
+
+
+
+/* Line 2053 of yacc.c  */
+#line 56 "/home/unicorn/devel-ssd/hexanoise-build/hexanoise/parser.hpp"
+
+/* Tokens.  */
+#ifndef YYTOKENTYPE
+# define YYTOKENTYPE
+   /* Put the tokens into the symbol table, so that GDB and other debuggers
+      know about them.  */
+   enum yytokentype {
+     TIDENTIFIER = 258,
+     TVALUE = 259,
+     TSTRING = 260,
+     TCOLON = 261,
+     TCOMMA = 262,
+     TLPAREN = 263,
+     TRPAREN = 264,
+     TLACCOL = 265,
+     TRACCOL = 266,
+     TDOLLAR = 267,
+     TAT = 268
+   };
+#endif
+
+
+#if ! defined YYSTYPE && ! defined YYSTYPE_IS_DECLARED
+typedef union YYSTYPE
+{
+/* Line 2053 of yacc.c  */
+#line 34 "parser.y"
+
+    hexa::noise::function* func;
+    std::vector<hexa::noise::function*>* param_list;
+    std::string* string;
+    int token;
+
+
+/* Line 2053 of yacc.c  */
+#line 92 "/home/unicorn/devel-ssd/hexanoise-build/hexanoise/parser.hpp"
+} YYSTYPE;
+# define YYSTYPE_IS_TRIVIAL 1
+# define yystype YYSTYPE /* obsolescent; will be withdrawn */
+# define YYSTYPE_IS_DECLARED 1
+#endif
+
+
+#ifdef YYPARSE_PARAM
+#if defined __STDC__ || defined __cplusplus
+int yyparse (void *YYPARSE_PARAM);
+#else
+int yyparse ();
+#endif
+#else /* ! YYPARSE_PARAM */
+#if defined __STDC__ || defined __cplusplus
+int yyparse (hexa::noise::function** func, yyscan_t scanner);
+#else
+int yyparse ();
+#endif
+#endif /* ! YYPARSE_PARAM */
+
+#endif /* !YY_YY_HOME_UNICORN_DEVEL_SSD_HEXANOISE_BUILD_HEXANOISE_PARSER_HPP_INCLUDED  */
